9d7b31db37a5e4821b4063e72d88914b558859eb
vadim-prog <58294789+vadim-prog@users.noreply.github.com>
2021-08-17 10:32:15 +0300

    17082021-10-31

---
+++ pars_hh/pars_hh/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pars_hh project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pars_hh.settings')
+
+application = get_wsgi_application()
