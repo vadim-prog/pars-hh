62a9be176614a21f7b065d755ad5c332c06fd396
vadim-prog <58294789+vadim-prog@users.noreply.github.com>
2021-08-17 20:00:34 +0300

    17082021-20-00

--- pars_hh/vacancy/views.py
+++ pars_hh/vacancy/views.py
@@ -1,17 +1,44 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from .forms import *
+
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        ]
 
 
 def index(request):
-    return HttpResponse('<h1>Главная страница</h1>')
+
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['vac_input'])
+            #form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+
+    return render(request, 'vacancy/index.html', {'form': form, 'menu': menu, 'title': 'HH_Parsing'})
+
 
 
 def contact(request):
-    return HttpResponse("Обратная связь")
+    context = {
+        'menu': menu,
+        'title': 'HH_Parsing',
+        # 'cat_selected': 0,
+    }
+    return render(request, 'vacancy/contact.html', context=context)
 
 
 def about(request):
-    return HttpResponse("О сайте")
+    context = {
+        'menu': menu,
+        'title': 'HH_Parsing',
+        # 'cat_selected': 0,
+    }
+    return render(request, 'vacancy/about.html', context=context)
 
 
 def pageNotFound(request, exception):
