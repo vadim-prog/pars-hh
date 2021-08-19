b6262bcb0b29ed7fce8d1fd1627aaf6df6908c08
vadim <plehanov101188@mail.ru>
2021-08-20 00:23:08 +0300

    20082021 00-22

--- pars_hh/vacancy/views.py
+++ pars_hh/vacancy/views.py
@@ -13,19 +13,21 @@ def index(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            # print(form.cleaned_data['vac_input'])
             res = Results(form.cleaned_data['vac_input'], form.cleaned_data['reg_input'])
             res_request = res.parsing()
 
-            if len(res_request) != 0:
-                for vac in range(0, len(res_request)):
-                    print(res_request[vac].vacancy)
-            return redirect('home')
-            #return render(request, 'vacancy/index.html', {'form': form, 'menu': menu, 'title': 'HH_Parsing'})
+            #if len(res_request) != 0:
+
+                #for vac in range(0, len(res_request)):
+                   # print(res_request[vac].vacancy)
+
+            # return redirect('home')
+            # return render(request, 'vacancy/index.html', {'form': form, 'menu': menu, 'title': 'HH_Parsing'})
     else:
+        res_request = []
         form = AddPostForm()
 
-    return render(request, 'vacancy/index.html', {'form': form, 'menu': menu, 'title': 'HH_Parsing'})
+    return render(request, 'vacancy/index.html', {'res_request': res_request, 'len_res_request': len(res_request),'form': form, 'menu': menu, 'title': 'HH_Parsing'})
 
 
 def results(request):
